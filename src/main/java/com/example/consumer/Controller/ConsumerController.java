54f1c2e8fc727e3c0dbbdb145d0bb5cd5ea203b7
Felipe-Cardona <felipe.cardona@pucrs.br>
2024-04-10 10:41:50 -0300

    creating base routes

--- src/main/java/com/example/consumer/Controller/ConsumerController.java
+++ src/main/java/com/example/consumer/Controller/ConsumerController.java
@@ -1,5 +1,45 @@
 package com.example.consumer.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("api/v1/consumer")
+@AllArgsConstructor
 public class ConsumerController {
-    
+    @PostMapping
+    public void registerConsumer() {
+
+    }
+
+    @PutMapping
+    public void modifyConsumer() {
+
+    }
+
+    @GetMapping
+    public void getConsumer() {
+
+    }
+
+    @PostMapping("/transactions")
+    public void makeTransaction() {
+
+    }
+
+    @PutMapping("/transactions/{transactionId}")
+    public void reimburse(@PathVariable("transactionId") String id) {
+
+    }
+
+    @GetMapping("/transactions")
+    public void getAllTransactions() {
+        
+    }
 }
--- src/main/java/com/example/consumer/Controller/PaymentController.java
+++ src/main/java/com/example/consumer/Controller/PaymentController.java
@@ -1,22 +1,30 @@
 package com.example.consumer.Controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
 
 @RestController
-@RequestMapping("/payment-method")
+@RequestMapping("api/v1/payment-methods")
 @AllArgsConstructor
 public class PaymentController {
-    @PostMapping("/")
-    @CrossOrigin("*")
+    @PostMapping
     public void addPaymentMethod() {
         
     }
 
-    public void 
+    @DeleteMapping("/{paymentMethodId}")
+    public void removePaymentMethod(@PathVariable("paymentMethodId") String id) {
+        
+    }
+
+    @GetMapping
+    public void getAllPaymentMethods() {
+    
+    }
 }
