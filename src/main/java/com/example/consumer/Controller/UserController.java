0fe66aa98cb3cec3dfccce8e6ee9f727ce235234
Felipe-Cardona <felipe.cardona@pucrs.br>
2024-04-10 11:32:47 -0300

    creating base dto and requestbody

--- src/main/java/com/example/consumer/Controller/UserController.java
+++ src/main/java/com/example/consumer/Controller/UserController.java
@@ -4,8 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.consumer.DTOs.RegisterUserDTO;
 
 import lombok.AllArgsConstructor;
 
@@ -14,8 +17,8 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class UserController {
     @PostMapping
-    public void registerConsumer() {
-
+    public RegisterUserDTO registerConsumer(@RequestBody RegisterUserDTO body) {
+        return body;
     }
 
     @PutMapping
--- src/main/java/com/example/consumer/DTOs/RegisterUserDTO.java
+++ src/main/java/com/example/consumer/DTOs/RegisterUserDTO.java
@@ -1,5 +1,14 @@
 package com.example.consumer.DTOs;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class RegisterUserDTO {
-    
+    private String name;
+    private String email;
+    private String firebaseToken;
+    private String tenantId;
+    private String privacyConfigId;
 }
